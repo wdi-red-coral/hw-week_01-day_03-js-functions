00a60f201b5c7944b46beb2cbca39b8c5d1c53f7
hamoghamdi <haneen.aljabrah@gmail.com>
2019-04-23 22:41:05 +0300

    Edit/fix the random numbers code lines, to limit in between 1-6.

--- dice.js
+++ dice.js
@@ -1,6 +1,6 @@
 const rollDice = function (){
-    let num1 = 1+ Math.floor(Math.random() * 7);
-    let num2 =1+ Math.floor(Math.random() * 7);
+    let num1 = 1+ Math.floor(Math.random() * 6);
+    let num2 =1+ Math.floor(Math.random() * 6);
     
     return "Dice rolled are " + num1 + " and " + num2 + ". Sum is " + (num1+num2);
 }
