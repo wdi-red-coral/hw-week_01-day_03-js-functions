f109112c61effc0220a42dc564bcc6bd5b4abc21
hamoghamdi <haneen.aljabrah@gmail.com>
2019-04-23 22:51:33 +0300

    Dice Roller Bonus Problem Solved!

--- dice.js
+++ dice.js
@@ -1,7 +1,18 @@
-const rollDice = function (){
-    let num1 = 1+ Math.floor(Math.random() * 6);
-    let num2 =1+ Math.floor(Math.random() * 6);
-    
-    return "Dice rolled are " + num1 + " and " + num2 + ". Sum is " + (num1+num2);
+// bonus: # of dice rolled
+const rollDice = function (diceRoll) {
+    let i = 0;
+    let diceValue = [];
+    do {
+        diceValue[i] = 1 + Math.floor(Math.random() * 6);
+        i++;
+    } while (i < diceRoll)
+
+    let sum = 0;
+    for (m = 0; m < diceValue.length; m++) {
+        sum = sum + diceValue[m];
+    }
+    return "Dice rolled are " + diceValue + ". Sum is " + sum;
 }
-rollDice();
+rollDice(3);
+rollDice(4);
+rollDice(9);
