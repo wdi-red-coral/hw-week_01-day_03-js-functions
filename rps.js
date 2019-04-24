8b3f5f9a0894d7b84cfb21511313a504038b368c
asmamk <i.amk94@hotmail.com>
2019-04-24 11:48:29 +0300

    i'm not understand the idea of the game

---
+++ rps.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Method 1
+
+Name:search()
+Description:method searches a string for a specified value 
+and returns the position of the match
+Example using it: 
+var str = "Please locate where 'locate' occurs!";
+var pos = str.search("locate");
+
+
+Method 2
+
+Name: indexOf()
+Description:method returns the index of (the position of) the first 
+occurrence of a specified text in a string
+Example using it:
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+
+
+Method 3
+
+Name:lastIndexOf()
+Description:method returns the index of the last occurrence 
+of a specified text in a string
+Example using it: 
+var str = "Please locate where 'locate' occurs!";
+var pos = str.lastIndexOf("locate");
