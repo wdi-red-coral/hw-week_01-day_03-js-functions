869f0daade961f91593c3f1f4459b6357b55854b
LamarZM <Lamar.Mufti@yahoo.com>
2019-04-24 11:41:17 +0300

    my rock paper scissors hw

---
+++ dice.js
@@ -0,0 +1,15 @@
+const rollDice1= function(){
+    let rollDice1= Math.floor(Math.random() * 6) + 1;  
+    return rollDice1
+    }
+    const rollDice2= function(){
+    let rollDice2 = Math.floor(Math.random() * 4) + 1;  
+    return rollDice2
+    }
+   
+    rollDice1()
+    //solution
+    //5
+    rollDice2()
+    //solution
+    //1
