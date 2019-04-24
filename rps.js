e60ddf863b13242bb782a7446efbda1a9496cf66
asmamk <49795130+asmamk@users.noreply.github.com>
2019-04-24 21:09:45 +0300

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,47 +1,22 @@
+const game = function (choice) {
 
+    let computer= Math.floor(Math.random() * 3);
 
+    if (computer===0) {
+        computer='rock'
+    }else if (computer===1){ 
+        computer='paper'
+    }else {
+        computer='scissors'
+    }      
+   if(computer === choice){
+        return "tie";
+   }else{
+		if (computer === "rock" && choice === "paper" || choice === "rock" && computer === "scissors" || computer === "paper" && choice === "sr" ){
+            return "user win";
+        } else {
+            return "computer win" ;
+        }
+    } 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Method 1
-
-Name:search()
-Description:method searches a string for a specified value 
-and returns the position of the match
-Example using it: 
-var str = "Please locate where 'locate' occurs!";
-var pos = str.search("locate");
-
-
-Method 2
-
-Name: indexOf()
-Description:method returns the index of (the position of) the first 
-occurrence of a specified text in a string
-Example using it:
-var str = "Please locate where 'locate' occurs!";
-var pos = str.indexOf("locate");
-
-
-Method 3
-
-Name:lastIndexOf()
-Description:method returns the index of the last occurrence 
-of a specified text in a string
-Example using it: 
-var str = "Please locate where 'locate' occurs!";
-var pos = str.lastIndexOf("locate");
